f744b91a881d1666a19dc97c5324e57adcfcf66f
Maha ALbugami <50253764+MahaBugami@users.noreply.github.com>
2023-05-30 09:56:54 +0300

    Add files via upload

---
+++ ApiCallSwiftApp.swift
@@ -0,0 +1,18 @@
+//
+//  ApiCallSwiftApp.swift
+//  ApiCallSwift
+//
+//  Created by MAHA on 09/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ApiCallSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+      ContentView()
+        }
+    }
+}
+
